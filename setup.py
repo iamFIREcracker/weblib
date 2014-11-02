35434a5ccfffcdccfe53eccdd963cf4681ebca07
Matteo Landi <matteo@matteolandi.net>
2014-11-02 16:09:43 -0500

    Lint, bitches

--- setup.py
+++ setup.py
@@ -2,25 +2,22 @@
 # -*- coding: utf-8 -*-
 
 
-import os
-from setuptools import find_packages
 from setuptools import setup
 
 
 def get_version():
     """Gets the repository version."""
     import subprocess
-    proc = subprocess.Popen(
-            'hg log -r tip --template "{latesttagdistance}"',
-            shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen('hg log -r tip --template "{latesttagdistance}"',
+                            shell=True,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     pending, _ = proc.communicate()
     return "%(tag)sd%(pending)s" % dict(tag='0.0.1', pending=pending)
 
 
-params = dict(
-    name='weblib',
-    version=get_version(),
-    packages=['weblib']
+PARAMS = dict(
 )
 
-setup(**params)
+setup(name='weblib',
+      version=get_version(),
+      packages=['weblib'])
