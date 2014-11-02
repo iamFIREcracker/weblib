0b25ed2ce3db0b5f3ecabe4736d7e81966644ac5
Matteo Landi <matteo@matteolandi.net>
2014-11-02 14:30:40 -0500

    Use hg revision to version the library

--- setup.py
+++ setup.py
@@ -7,12 +7,23 @@ from setuptools import find_packages
 from setuptools import setup
 
 
+def get_version():
+    """Gets the repository version."""
+    import subprocess
+    proc = subprocess.Popen(
+            'hg log -r tip --template "{latesttagdistance}"',
+            shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    pending, _ = proc.communicate()
+    return "%(tag)sd%(pending)s" % dict(tag='0.0.1', pending=pending)
+
+
 requirements = os.path.join(os.path.dirname(__file__), 'requirements.txt')
 INSTALL_REQUIRES = open(requirements).read().split()
 
 
 params = dict(
     name='weblib',
+    version=get_version(),
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
 )
