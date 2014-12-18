e5209b437e7c2107ebd0422f0e6b4684ac4b9d54
Matteo Landi <matteo@matteolandi.net>
2014-12-18 01:16:14 +0100

    Move dependencies inside setup.py and out of the requirements file

--- setup.py
+++ setup.py
@@ -18,5 +18,12 @@ def get_version():
 setup(name='weblib',
       version=get_version(),
       packages=['weblib'],
-      install_requires=[req for req in open('requirements.txt').readlines()
-                        if '-e' not in req])
+      install_requires=[
+          'Jinja2==2.7.3',
+          'SQLAlchemy==0.9.8',
+          'celery==3.1.17',
+          'oauth2==1.5.211',
+          'redis==2.10.3',
+          'web.py==0.37',
+          'webassets==0.10.1',
+      ])
