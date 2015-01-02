9b18f0b2f1cab398db74fabbc4f6eabc37924486
Matteo Landi <matteo@matteolandi.net>
2015-01-02 18:03:38 +0100

    export custom render_jinja

--- weblib/app_processors.py
+++ weblib/app_processors.py
@@ -4,8 +4,6 @@
 import web
 
 from weblib.logging import create_logger
-from jinja2 import TemplateNotFound
-from jinja2 import TemplatesNotFound
 
 
 def load_logger():
@@ -19,30 +17,30 @@ def load_path_url():
     web.ctx.path_url = web.ctx.home + web.ctx.path
 
 
+class render_jinja:
+    """Rendering interface to Jinja2 Templates
+
+    Example:
+
+        render= render_jinja('templates')
+        render.hello(name='jinja2')
+        """
+    def __init__(self, *a, **kwargs):
+        extensions = kwargs.pop('extensions', [])
+        globals = kwargs.pop('globals', {})
+
+        from jinja2 import Environment,FileSystemLoader
+        self._lookup = Environment(loader=FileSystemLoader(*a, **kwargs), extensions=extensions)
+        self._lookup.globals.update(globals)
+
+    def __getattr__(self, name):
+        paths = [name + '.' + ext for ext in ['html', 'xml']]
+        t = self._lookup.select_template(paths)
+        return t.render
+
+
 def load_render(views, **globals):
     '''Add the renderer to the shared context.'''
-
-    class render_jinja:
-        """Rendering interface to Jinja2 Templates
-
-        Example:
-
-            render= render_jinja('templates')
-            render.hello(name='jinja2')
-            """
-        def __init__(self, *a, **kwargs):
-            extensions = kwargs.pop('extensions', [])
-            globals = kwargs.pop('globals', {})
-
-            from jinja2 import Environment,FileSystemLoader
-            self._lookup = Environment(loader=FileSystemLoader(*a, **kwargs), extensions=extensions)
-            self._lookup.globals.update(globals)
-
-        def __getattr__(self, name):
-            paths = [name + '.' + ext for ext in ['html', 'xml']]
-            t = self._lookup.select_template(paths)
-            return t.render
-
     render = render_jinja(views, encoding='utf-8',
                           extensions=['jinja2.ext.do'],
                           globals=globals)
@@ -59,8 +57,9 @@ def load_render_with_assets(views, env, **globals):
                           extensions=['jinja2.ext.do', AssetsExtension])
     render._lookup.assets_environment = env
     render._lookup.globals.update(globals)
+
     def inner():
-        web.ctx.render = render;
+        web.ctx.render = render
     return inner
 
 
