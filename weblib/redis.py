fa68f5b3870d12a7a5e9b418f9fd64c6840ccd73
Matteo Landi <matteo@matteolandi.net>
2015-01-11 10:36:44 +0100

    change redis config variable again

--- weblib/redis.py
+++ weblib/redis.py
@@ -9,9 +9,9 @@ import web
 __all__ = ['create_redis']
 
 
-def create_redis(host=None, port=None):
-    if host is None:
-        host = web.config.get('REDIS_HOST', 'localhost')
+def create_redis(address=None, port=None):
+    if address is None:
+        address = web.config.get('REDIS_ADDRESS', '127.0.0.1')
     if port is None:
         port = web.config.get('REDIS_PORT', 6379)
-    return redis.Redis(host=host, port=port)
+    return redis.Redis(host=address, port=port)
