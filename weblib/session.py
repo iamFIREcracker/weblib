fa8ff7992c28533f06c07c6a379cafa15dd74fda
Matteo Landi <matteo@matteolandi.net>
2015-02-14 20:11:13 +0100

    More redis store improvements

--- weblib/session.py
+++ weblib/session.py
@@ -5,14 +5,6 @@ import datetime
 from web.session import Store
 
 
-def _redis_key(key):
-    return 'webpy.session' + key
-
-
-def _datetime_to_int(dt):
-    return int(float(dt.strftme('%s.%f') * 1000000))
-
-
 class RedisStore(Store):
     """Store for saving a session in Redis.
 
@@ -22,34 +14,41 @@ class RedisStore(Store):
     def __init__(self, redis):
         self._redis = redis
 
+    def _redis_key(self, key):
+        return 'webpy.session:' + key
+
+    def _datetime_to_float(self, dt):
+        return int(float(dt.strftime('%s.%f')) * 1000000)
+
     def __contains__(self, key):
-        return self._redis.get(_redis_key(key)) is not None
+        return self._redis.get(self._redis_key(key)) is not None
 
     def __getitem__(self, key):
-        data = self.decode(self._redis.get(_redis_key(key)))
+        data = self.decode(self._redis.get(self._redis_key(key)))
         value = data['value']
-        now_int = data['now_int']
-        self._redis.zrem('webpy.sessions', now_int)
+        now = data['now']
+        self._redis.zrem('webpy.sessions', now)
         self[key] = value
         return value
 
     def __setitem__(self, key, value):
-        redis_key = _redis_key(key)
-        now_int = _datetime_to_int(datetime.datetime.now())
-        data = self.encode(dict(value=value, now_int=now_int))
+        redis_key = self._redis_key(key)
+        now = self._datetime_to_float(datetime.datetime.now())
+        data = self.encode(dict(value=value, now=now))
         self._redis.set(redis_key, data)
-        self._redis.zadd('webpy.sessions', now_int, key)
+        self._redis.zadd('webpy.sessions', key, now)
 
     def __delitem__(self, key):
-        self._redis.delete(_redis_key(key))
+        self._redis.delete(self._redis_key(key))
+        self._redis.zrem('webpy.sessions', key)
 
     def cleanup(self, timeout):
-        seconds_to_days = 24.0 * 60 * 60
-        timeout = datetime.timedelta(timeout / seconds_to_days)
-        last_allowed_time = datetime.datetime.now() - timeout
-        last_allowed_time_int = _datetime_to_int(last_allowed_time)
-        for (key, now_int) in self._redis.zrange('webpy.sessions', 0,
-                                                 last_allowed_time_int,
-                                                 withscores=True):
-            self._redis.zrem('webpy.sessions', now_int)
+        last_allowed_time = \
+            datetime.datetime.now() - datetime.timedelta(seconds=timeout)
+        last_allowed_time = self._datetime_to_float(last_allowed_time)
+        for (key, now) in self._redis.zrangebyscore('webpy.sessions',
+                                                    0,
+                                                    last_allowed_time,
+                                                    score_cast_func=int,
+                                                    withscores=True):
             del self[key]
