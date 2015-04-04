f14693a0c1c006fa16badb44c444b797e56f189b
Matteo Landi <matteo@matteolandi.net>
2015-04-04 11:24:42 -0400

    add default internalerror handler

--- weblib/__init__.py
+++ weblib/__init__.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import web
 from web.webapi import _status_code
 from web.webapi import Redirect
 
+from weblib.logging import create_logger
+
 
 nocontent = NoContent = _status_code("204 No Content")
+
 
 class Created(Redirect):
     """A `201 Created` response."""
@@ -13,3 +17,21 @@ class Created(Redirect):
         Redirect.__init__(self, url, '201 Created', absolute=absolute)
 
 created = Created
+
+
+def internalerror(title):
+    def inner():
+        message = '''
+%(title)s\n
+method: %(method)s
+fullpath: %(fullpath)s
+env: %(env)s
+
+''' % dict(title=title,
+           method=web.ctx.method,
+           fullpath=web.ctx.fullpath,
+           env=web.ctx.environ
+           )
+        create_logger().exception(message)
+        raise web.internalerror('Holy shit!')
+    return inner
