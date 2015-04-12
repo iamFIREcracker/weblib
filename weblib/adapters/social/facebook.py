29b8012abb6151b2f802a9892fde5da37d3674e7
Matteo Landi <matteo@matteolandi.net>
2015-04-12 09:28:05 -0400

    Move facebook stuff into separate module, and add helpers to cache mutual friends

--- weblib/adapters/social/facebook.py
+++ weblib/adapters/social/facebook.py
@@ -3,9 +3,11 @@
 
 import hashlib
 import hmac
+import json
 import urllib2
 
 import requests
+from weblib.pubsub import Publisher
 
 
 class FacebookAdapter(object):
@@ -56,3 +58,65 @@ class FacebookAdapter(object):
             return (None, ('Invalid Facebook response', url, str(data)))
         except urllib2.HTTPError as e:
             return (None, ('Unable to contact the server', url, str(e)))
+
+
+class FacebookProfileGetter(Publisher):
+    def perform(self, adapter, access_token):
+        """Gets the profile information of the user identified by the specified
+        access token.
+
+        If something goes wrong while trying to access the profile info, a
+        'profile_not_found' message will be published with the error cause;
+        on the other hand, a 'profile_found' message containing the profile
+        information will be sent back to subscribers."""
+        (data, error) = adapter.profile(access_token)
+        if error is not None:
+            self.publish('profile_not_found', error)
+        else:
+            data['name'] = adapter.name(data['first_name'], data['last_name'])
+            data['avatar_unresolved'] = adapter.avatar_unresolved(data['id'])
+            data['avatar'] = adapter.avatar(data['id'])
+            data['email'] = (data['email'] if 'email' in data
+                             else 'fuckyou@stupid.api')
+            self.publish('profile_found', data)
+
+
+class FacebookMutualFriendsGetter(Publisher):
+    def perform(self, adapter, app_secret, access_token, other_user_id):
+        (data, error) = adapter.mutual_friends(app_secret,
+                                               access_token,
+                                               other_user_id)
+        if error is not None:
+            self.publish('mutual_friends_not_found', error)
+        else:
+            mutual_friends = data['context']['mutual_friends']
+            summary = \
+                dict(total_count=mutual_friends['summary']['total_count'])
+            data = [dict(name=adapter.name(d['first_name'],
+                                           d['last_name']),
+                         avatar=d['picture']['data']['url'])
+                    for d in mutual_friends['data']]
+            self.publish('mutual_friends_found',
+                         dict(summary=summary, data=data))
+
+
+def _facebook_mutualfriends_cacheid(user_id, other_user_id):
+    return 'facebook_mutualfriends:%s_%s' % (user_id, other_user_id)
+
+
+class CachedFacebookMutualFriendsGetter(Publisher):
+    def perform(self, redis, user_id, other_user_id):
+        cache_id = _facebook_mutualfriends_cacheid(user_id, other_user_id)
+        raw = redis.get(cache_id)
+        if raw is None:
+            self.publish('cached_mutual_friends_not_found', id)
+        else:
+            self.publish('cached_mutual_friends_found', json.loads(raw))
+
+
+class CachedFacebookMutualFriendsSetter(Publisher):
+    def perform(self, redis, user_id, other_user_id, mutual_friends,
+                expire):
+        cache_id = _facebook_mutualfriends_cacheid(user_id, other_user_id)
+        redis.set(cache_id, json.dumps(mutual_friends), ex=expire)
+        self.publish('cached_mutual_friends_set', cache_id)
